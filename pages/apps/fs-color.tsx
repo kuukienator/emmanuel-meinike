f141e251d7f08847f47a9083b81c781472f87baf
Emmanuel Meinike <emmanuel.meinike@gmail.com>
2021-12-11 12:25:57 +0100

    add color input and picker

--- pages/apps/fs-color.tsx
+++ pages/apps/fs-color.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import clsx from 'clsx';
 
 import Color from '../../lib/color';
+import ColorPickerIcon from '../../assets/svgs/source_icons_color-picker.svg';
 
 const toggleFullscreen = () => {
   if (!document.fullscreenElement) {
@@ -14,7 +15,9 @@ const toggleFullscreen = () => {
 };
 
 const FSColor = () => {
-  const [color, setColor] = useState<Color>(Color.random());
+  const [color, setColor] = useState<Color>(new Color(0, 0, 0));
+  const [currentInput, setCurrentInput] = useState<string>(color.toHex());
+  const [isInputFoccused, toggleIsInputFocused] = useState<boolean>(false);
   const [isUIHidden, setIsUIHidden] = useState(false);
 
   useEffect(() => {
@@ -32,6 +35,13 @@ const FSColor = () => {
     };
   }, []);
 
+  useEffect(() => {
+    try {
+      const color = Color.fromHex(currentInput);
+      setColor(color);
+    } catch (e) {}
+  }, [currentInput]);
+
   return (
     <div
       className="w-screen h-screen flex justify-center items-center flex-col text-2xl"
@@ -43,8 +53,14 @@ const FSColor = () => {
       }}
       tabIndex={0}
       onKeyDown={(e) => {
+        if (isInputFoccused) {
+          return;
+        }
+
         if (e.key === 'r') {
-          setColor(Color.random());
+          const color = Color.random();
+          setColor(color);
+          setCurrentInput(color.toHex());
         }
         if (e.key === 'f') {
           toggleFullscreen();
@@ -64,16 +80,35 @@ const FSColor = () => {
           hidden: isUIHidden,
         })}
       >
-        <p
-          className="text-6xl border-8 p-4 rounded-2xl mb-8 w-96 text-center font-bold"
+        <div
+          className="flex flex-row justify-center items-center mb-8 border-8 rounded-2xl p-4"
           style={{
             borderColor: color.isDark()
               ? color.lighten(15).toHSLA()
               : color.darken(15).toHSLA(),
           }}
         >
-          {color.toHex()}
-        </p>
+          <input
+            className="text-6xl w-96 text-center font-bold bg-inherit"
+            onFocus={() => toggleIsInputFocused(true)}
+            onBlur={() => toggleIsInputFocused(false)}
+            value={currentInput}
+            onChange={(e) => setCurrentInput(e.target.value)}
+          />
+          <label className="cursor-pointer">
+            <ColorPickerIcon className="text-6xl" />
+            <input
+              type="color"
+              onChange={(e) => {
+                const color = Color.fromHex(e.target.value);
+                setColor(color);
+                setCurrentInput(color.toHex());
+              }}
+              className="hidden"
+            />
+          </label>
+        </div>
+
         <p>
           Press <span className="font-bold">R</span> for random.
         </p>

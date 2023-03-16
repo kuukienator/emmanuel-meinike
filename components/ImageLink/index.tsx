78ca436668e40807f7334e5e6ba2079f53c620ec
Emmanuel Meinike <emmanuel.meinike@gmail.com>
2023-03-16 19:43:09 +0100

    Update major packages to latest versions

--- components/ImageLink/index.tsx
+++ components/ImageLink/index.tsx
@@ -29,23 +29,21 @@ export const ImageLink: FC<Props> = ({
 }) => (
   <div className="p-2 flex flex-col overflow-hidden sm:w-1/2 lg:w-1/3 xl:w-1/4 mb-6 text-copy justify-between items-end">
     <div className="border-b-4 border-primary sm:border-none">
-      <Link href={url}>
-        <a
-          target="_blank"
-          className="block overflow-hidden"
-          rel="noopener noreferrer"
-          href={url}
-        >
-          <img
-            className="transform transition duration-50 hover:scale-110 hover:opacity-50"
-            src={image}
-            srcSet={createSrcSet(images)}
-            sizes="(min-width: 1280px) 25vw, (min-width: 1024px) 33vw, (min-width: 640px) 50vw, 90vw"
-            alt={altText}
-            loading="lazy"
-            style={{ height: '300px', objectFit: 'cover', width: '100%' }}
-          />
-        </a>
+      <Link
+        href={url}
+        target="_blank"
+        className="block overflow-hidden"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="transform transition duration-50 hover:scale-110 hover:opacity-50"
+          src={image}
+          srcSet={createSrcSet(images)}
+          sizes="(min-width: 1280px) 25vw, (min-width: 1024px) 33vw, (min-width: 640px) 50vw, 90vw"
+          alt={altText}
+          loading="lazy"
+          style={{ height: '300px', objectFit: 'cover', width: '100%' }}
+        />
       </Link>
       <div className="py-2 h-24">
         <p className="font-bold text-lg">{title}</p>

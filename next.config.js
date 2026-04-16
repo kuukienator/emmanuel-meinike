029a68553202f2e3c3ce8ce17a9f61e961c1bdd2
kuukienator <emmanuel.meinike@gmail.com>
2026-04-16 11:52:03 +0200

    Remove `@svgr/webpack` and related configuration; replace SVG import with inline SVG component in `fs-color`

--- next.config.js
+++ next.config.js
@@ -4,25 +4,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: [{ loader: '@svgr/webpack' }],
-        as: '*.js',
-      },
-    },
-  },
   output: 'export',
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
